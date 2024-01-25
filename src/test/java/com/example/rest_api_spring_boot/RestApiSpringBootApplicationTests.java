c95734fbc7b8b74ad6c8fcc68e5b12bf9b5af2d2
Rahul Nayak <rahul_nayak@thbs.india.com>
2024-01-25 13:48:26 +0530

    created jar file

--- src/test/java/com/example/rest_api_spring_boot/RestApiSpringBootApplicationTests.java
+++ src/test/java/com/example/rest_api_spring_boot/RestApiSpringBootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.rest_api_spring_boot;
+// package com.example.rest_api_spring_boot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class RestApiSpringBootApplicationTests {
+// @SpringBootTest
+// class RestApiSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
